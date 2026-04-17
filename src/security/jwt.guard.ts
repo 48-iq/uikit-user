f564caf4d70aeefb3a1d72973c896e55f90ec319
48-iq <ivn25ov@mail.ru>
2026-04-17 16:04:22 +0400

    public decorator

--- src/security/jwt.guard.ts
+++ src/security/jwt.guard.ts
@@ -2,14 +2,24 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService, private reflector: Reflector) {}
   async canActivate(
     context: ExecutionContext,
   ): Promise<boolean>  {
     const request = context.switchToHttp().getRequest();
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      // 💡 See this condition
+      return true;
+    }
     const jwt = request.headers.authorization;
     const token = this.extractTokenFromHeader(request);
     if (!token) return false;
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,26 @@
-import { Body, Controller, Get, Param, Post, Put, Query, Req } from '@nestjs/common';
-import { UserCreateDto, UserEntityDto, UserUpdateDto } from '@48-iq/uikit-dto-lib';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+} from '@nestjs/common';
+import {
+  UserCreateDto,
+  UserEntityDto,
+  UserUpdateDto,
+} from '@48-iq/uikit-dto-lib';
 import { UserService } from './services/user.service';
+import { Public } from 'src/security/public.decorator';
 @Controller('/api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post()
+  @Public()
   async createUser(@Body() body: UserCreateDto) {
     await this.userService.create({
       id: body.username,
@@ -13,24 +29,19 @@ export class UserController {
   }
 
   @Get('/:id')
-  async getUser(
-    @Param('id') id: string,
-  ) {
+  async getUser(@Param('id') id: string) {
     const user = await this.userService.get(id);
     const userEntityDto = new UserEntityDto({
       id: user.id,
       email: user.email,
       createdAt: user.createdAt.toISOString(),
-      updatedAt: user.updatedAt?.toISOString()??"none",
+      updatedAt: user.updatedAt?.toISOString() ?? 'none',
     });
     return userEntityDto;
   }
 
   @Put()
-  async updateUser(
-    @Req() request: Request,
-    @Body() body: UserUpdateDto
-  ) {
+  async updateUser(@Req() request: Request, @Body() body: UserUpdateDto) {
     const id = request['authPayload'].id;
     await this.userService.update(id, { email: body.email });
   }
