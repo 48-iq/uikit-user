f40dc7680d0231decd4ac3bfbbe91dbc390e129f
dissemblee <kusainovdavid@gmail.com>
2026-04-28 22:11:43 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const corsOrigin = configService.getOrThrow<string>('CORS_ORIGIN');
+
+  app.enableCors({
+    "origin": corsOrigin,
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "credentials": true,
+    "allowedHeaders": "Content-Type, Accept, Authorization",
+    "optionsSuccessStatus": 204
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
